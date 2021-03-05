eaedcc113bfff7b4723ef46e73618c9fb3bf4084
manuel couto pintos <manuel.couto1@udc.es>
2021-03-05 01:13:13 +0100

    [custom_groups]

---
+++ src/custom_groups.py
@@ -0,0 +1,21 @@
+from pygame import Surface
+from pygame.sprite import Group
+
+
+class CustomGroup(Group):
+    def __init__(self, *sprites):
+        super().__init__(*sprites)
+
+    def draw(self, surface: Surface, camera_rect) -> None:
+        sprites = self.sprites()
+        if hasattr(surface, "blits"):
+            self.spritedict.update(
+                zip(
+                    sprites,
+                    surface.blits((spr.image, spr.rect.move(camera_rect)) for spr in sprites)
+                )
+            )
+        else:
+            for spr in sprites:
+                self.spritedict[spr] = surface.blit(spr.image, spr.rect)
+        self.lostsprites = []
